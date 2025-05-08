8c6cfac9adea5fae673d674bb2c0f8f548c2c7b4
Vladislav Yarmak <vladislav@vm-0.com>
2025-05-08 16:56:51 +0300

    initial tests

---
+++ unicmp_test.go
@@ -0,0 +1,53 @@
+package unicmp
+
+import (
+	"math/rand/v2"
+	"slices"
+	"testing"
+)
+
+func randBytes(n uint) []byte {
+	s := make([]byte, n)
+	for i := range s {
+		s[i] = byte(rand.IntN(int(^byte(0)) + 1))
+	}
+	return s
+}
+
+func sum(s []byte) int {
+	sum := 0
+	for _, v := range s {
+		sum += int(v)
+	}
+	return sum
+}
+
+func shuffle(s []byte) {
+	rand.Shuffle(len(s), func(i, j int) {
+		s[i], s[j] = s[j], s[i]
+	})
+}
+
+func TestSmoke(t *testing.T) {
+	orig := randBytes(10000)
+	s1 := slices.Clone(orig)
+	s2 := slices.Clone(orig)
+	if a, b := sum(s1), sum(s2); a != b {
+		t.Fatalf("sum(s1) != sum(s2): %d != %d", a, b)
+	}
+	shuffle(s1)
+	shuffle(s2)
+	if a, b := sum(s1), sum(s2); a != b {
+		t.Fatalf("sum(s1) != sum(s2): %d != %d", a, b)
+	} else {
+		if c := sum(orig); a != c {
+			t.Fatalf("sum(s1) != sum(orig): %d != %d", a, c)
+		}
+	}
+	o := ForType[byte]()
+	slices.SortFunc(s1, o.Cmp)
+	slices.SortFunc(s2, o.Cmp)
+	if !slices.Equal(s1, s2) {
+		t.Error("s1 content is not the same as s2 after sorting")
+	}
+}
