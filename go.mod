b030d5e28ae41692e3e38a8959863a78d63258ce
Vladislav Yarmak <vladislav@vm-0.com>
2025-05-08 16:20:03 +0300

    initial code

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/Snawoot/unicmp
 
 go 1.24.2
+
+require github.com/Snawoot/maphash v0.0.0-20250508122243-aa886187614d // indirect
---
+++ unicmp.go
@@ -0,0 +1,29 @@
+package unicmp
+
+import (
+	"cmp"
+
+	"github.com/Snawoot/maphash"
+)
+
+const maxRounds = 50
+
+type Ordering[T comparable] struct {
+	h maphash.Hasher[T]
+}
+
+func ForType[T comparable]() Ordering[T] {
+	return Ordering[T]{maphash.NewHasher[T](maphash.NewSeed(0))}
+}
+
+func (o Ordering[T]) Cmp(x, y T) int {
+	if x == y {
+		return 0
+	}
+	h := o.h
+	h1, h2 := h.Hash2(x), h.Hash2(y)
+	for i := uintptr(1); h1 == h2 && i < maxRounds; h1, h2, i = h.Hash2(x), h.Hash2(y), i+1 {
+		h = h.WithSeed(maphash.NewSeed(i))
+	}
+	return cmp.Compare(h1, h2)
+}
